a12922bbf6dcf2092bfb12fc7db5b546f899f5d1
“divsb” <talktodeviusa@gmail.com>
2023-10-08 23:20:35 -0400

    First Commit

---
+++ src/test/java/HelloWorld.java
@@ -0,0 +1,19 @@
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		
+		System.out.println("This is first program");
+		
+		String hungry ="yes";
+		
+		if(hungry=="no") 
+			System.out.println("Eat something");
+			else {
+				System.out.println("Do your work");
+			}
+		}
+	}
+
+
+
